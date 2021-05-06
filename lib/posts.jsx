bb7e6f9beda9a329aff3c125742215d59907869c
wanko <kouji124@w4.dion.ne.jp>
2021-05-06 16:43:51 -0700

    dynamic routing

--- lib/posts.jsx
+++ lib/posts.jsx
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import remark from 'remark'
+import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), "posts");
 // console.log("POSTS DIRECTORY: ", postsDirectory); // --> /Users/Koji/LearnCoding/nextjs-01practice/posts
@@ -62,7 +64,7 @@ export function getAllPostIds() {
   });
 }
 
-export function getPostData(id) {
+export async function getPostData(id) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   // console.log("FULL PATH: ", fullPath); // -->  /Users/Koji/LearnCoding/nextjs-01practice/posts/ssg-ssr.md
@@ -71,9 +73,16 @@ export function getPostData(id) {
   // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents);
 
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content)
+  const contentHtml = processedContent.toString()
+
   // Combine the data with the id
   return {
     id,
-    ...matterResult.data,
-  };
+    contentHtml,
+    ...matterResult.data
+  }
 }
